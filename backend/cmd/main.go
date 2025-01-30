45bb2f4e448fe8035f0f418f6c96caa3c2ebbdf8
SarahVestergren <sscv@itu.dk>
2025-01-30 14:52:30 +0100

    parse of gitlog

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 var repoPath string 
 
 func callGitLog() string{
-	var script = `git -C %s log --pretty=format:"%%h %%an %%as " --numstat`
+	var script = `git -C %s log --pretty=format:"%%h %%as %%ae %%(trailers:key=Co-authored-by,valueonly,separator="-")" --numstat --no-merges`
 
 	var cmd = exec.Command("bash", "-c", fmt.Sprintf(script, repoPath))
 
@@ -21,6 +22,46 @@ func callGitLog() string{
 	return string(output)
 }
 
+/* func parseCoAuthors (author string){
+
+} */
+
+
+func parseGitLog (lines string) [][] string {
+	var commitSha, timestamp, author, fileName, lineAdd, lineRemove string 
+	var blockLineCount int 
+	var result = [][]string{}
+
+	for _, l := range strings.Split (lines, "\n") {
+		var lineContent = strings.TrimSpace(l) 
+
+		if lineContent != "" {
+			if blockLineCount == 0 {
+				var fields = strings.Fields(lineContent)
+				commitSha, timestamp, author = fields[0], fields[1], fields[2]
+			} else {
+				var fields = strings.Fields(lineContent)
+				lineAdd, lineRemove, fileName =  fields[0], fields[1], fields[2]
+				result = append (result, []string{commitSha, timestamp, author, lineAdd, lineRemove, fileName})
+
+			}
+			blockLineCount++ 
+		}
+		
+
+		if lineContent == "" {
+			blockLineCount = 0
+
+		}
+	}
+	return result
+} 
+
+
+
+
+
+
 func main() {
 	repoPath  = os.Args[1]
 
@@ -28,3 +69,4 @@ func main() {
 
 	fmt.Print(rawData)
 }
+
