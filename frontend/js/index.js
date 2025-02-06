bc39ccfc2012262147693a1fc3523b00a911e3fe
SarahVestergren <sscv@itu.dk>
2025-02-06 15:19:36 +0100

    Frontend

---
+++ frontend/js/index.js
@@ -0,0 +1,8 @@
+var width = 800; 
+var height = 800; 
+
+var myCanvas = d3.select("#vis")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .style("background-color", "brown"); 
