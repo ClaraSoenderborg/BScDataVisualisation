055ba1aeb1b35f3bff2bc650afe7f36d122540e4
Julia <jukl@itu.dk>
2025-03-06 14:52:03 +0100

    commandline more details

--- main.go
+++ main.go
@@ -3,38 +3,51 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 )
 
-
+// default version. Overwritten at buildtime
+var version = "dev"
 
 func main() {
 
-	var repoPath = flag.String( "repoPath", "default", "help message hihi")
-	var dataLocation = flag.String( "dataLocation", "", "help message hihi")
+	var repoPath = flag.String("repoPath", "", "Mandatory: Path to repository")
+	var dataLocation = flag.String("dataLocation", "", "Optional: Path to save data in csv-file.\nOtherwise serves data in memory")
+	var versionFlag = flag.Bool("version", false, "Show version")
+	flag.Usage = func() {
+		fmt.Printf(`Usage:
+... [-repoPath <path_to_repository>] [-dataLocation <path_to_data>]
+... -h --help
+... --version
 
-	flag.Usage = func () {
-		fmt.Printf("usage \n")
+Options:` + "\n" + `
+  -h --help
+        Show this screen.` + "\n")
 		flag.PrintDefaults()
 	}
-	flag.Parse()
 
+	flag.Parse()
+	if *versionFlag {
+		//printVersion()
+		fmt.Println("Version:", version)
+		os.Exit(0)
+	}
 	var rawData = callGitLog(*repoPath)
 	var res = parseGitLog(rawData)
 	//parseToCSV(res)
-	if (*dataLocation != ""){
+	if *dataLocation != "" {
 		var fixedPath = strings.TrimSuffix(*dataLocation, "/") + "/data.csv"
 		fmt.Printf("Saving data file at: " + fixedPath + "\n")
 
 		writeToCSVFile(res, fixedPath)
 		setUpServer(fixedPath, nil)
-		
+
 	} else {
 		setUpServer("", res)
 	}
-	
+
 	//fmt.Printf("%v", res)
 	//fmt.Print(rawData)
 
-	
 }
