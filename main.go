af1801d33a2f49688f1de6a067c10923300e3577
Julia <jukl@itu.dk>
2025-04-03 14:08:42 +0200

    added datapath

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
@@ -15,6 +16,7 @@ var version = "dev"
 func main() {
 	// argument flags
 	var repoPath = flag.String("repoPath", "", "Mandatory: Path to repository. If more than one then seperate by comma without spacing")
+	var dataPath = flag.String("dataPath", "", "Mandatory: Path to csv file. If more than one then seperate by comma without spacing.\nThe csvfile needs the following syntax: commitSHA,date,week,author,linesAdded,linesDeleted,yAxis,nodeSize,fileName,repoPath\nExample: b55a306,2023-09-12,37,mail@itu.dk,2,6,8,1,Project/Program.cs,/Users/Desktop/")
 	var dataLocation = flag.String("dataLocation", "", "Optional: Path to save data in csv-file.\nOtherwise serves data in memory")
 	var versionFlag = flag.Bool("version", false, "Show version")
 	var excludeFile = flag.String("excludeFile", "", "RegExp on file names to exclude")
@@ -23,6 +25,7 @@ func main() {
 	var yAxis = flag.String("yAxis", "", "Mandatory: Metric for y-axis")
 	var nodeSize = flag.String("nodeSize", "", "Mandatory: Metric for node size")
 	var numberOfFiles = flag.String("numberOfFiles", "10", "Optional: Number of files per week")
+
 
 
 
@@ -47,6 +50,10 @@ Options:` + "\n" + `
 		os.Exit(0)
 	}
 
+	if *repoPath == "" && *dataPath == "" || *repoPath != "" && *dataPath != ""{
+		log.Fatal("Error: Either -repoPath or -dataPath must be provided.")
+	}
+
 	var metricOptions = [] string{"churn", "growth", "commit"}
 	if(!slices.Contains(metricOptions, *yAxis)){
 		log.Fatal("yAxis argument must be churn, growth or commit")
@@ -57,19 +64,62 @@ Options:` + "\n" + `
 
 	var metadata = map[string]string{"numberOfFiles":*numberOfFiles, "yAxis":*yAxis, "nodeSize":*nodeSize}
 
-	var allRepos[] string = strings.Split(*repoPath, ",")
+
 
 
 	var res [][] string
-	for _, item := range allRepos{
-	var rawData = callGitLog(item)
-	parsedData := parseGitLog(rawData, *excludeFile, *excludePath, *excludeKind, *yAxis, *nodeSize, item)
+	if *repoPath != "" && *dataPath == ""{
+		var allRepos[] string = strings.Split(*repoPath, ",")
 
-	for _, row := range parsedData {
-        res = append(res, row)
-    }
 
+		for _, item := range allRepos{
+			var rawData = callGitLog(item)
+			parsedData := parseGitLog(rawData, *excludeFile, *excludePath, *excludeKind, *yAxis, *nodeSize, item)
+			fmt.Println("hej1")
+
+			for _, row := range parsedData {
+				res = append(res, row)
+			}
+		}
+
+	} else if *repoPath == "" && *dataPath != ""{
+		var allData[] string = strings.Split(*dataPath, ",")
+
+		for _, item := range allData {
+			f, err := os.Open(item)
+			if err != nil {
+				log.Fatal("Unable to read input file " + item, err)
+			}
+
+			fmt.Println("Reading file:", item)
+
+			csvReader := csv.NewReader(f)
+			records, err := csvReader.ReadAll()
+
+			if err != nil {
+				log.Fatal("Unable to parse file as CSV for " + item, err)
+			}
+
+			// Log each record to inspect the number of fields
+			for i, record := range records {
+				fmt.Printf("Record %d: %v (Fields: %d)\n", i+1, record, len(record))
+				if len(record) != 10 {  // Check if the record has the expected number of columns
+					fmt.Printf("Warning: Record %d has incorrect number of fields: %d\n", i+1, len(record))
+				}
+			}
+
+			defer f.Close()
+
+			// If valid, append the records to the result
+			for _, record := range records {
+				res = append(res, record)
+				fmt.Println(record)
+
+			}
+		}
 	}
+
+
 		// if data should be saved locally at specified path
 		if *dataLocation != "" {
 			var fixedPath = strings.TrimSuffix(*dataLocation, "/") + "/data.csv"
