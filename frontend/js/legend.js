c17392f260695825c9ffdf8bcd64c1c644ea85b2
Clara Sønderborg <auso@itu.dk>
2025-04-21 16:08:13 +0200

    fonts and legend padding

--- frontend/js/legend.js
+++ frontend/js/legend.js
@@ -1,17 +1,17 @@
 const createLegend = (data) => {
     const authors = colorScale.domain()
-    const rowHeight = 30; 
+    const rowHeight = 30 
 
     const div = d3.select("#legendDiv")
     const legend = div.append("svg")
 
     const drawLegend = () => {
-        let usedRows = 1;
-        let xPosition = margin.left;
-        let yPosition = margin.top + 5;
+        let usedRows = 1
+        let xPosition = margin.left * 1.25
+        let yPosition = margin.top + 5
 
-        legend.selectAll(".legend-item").remove();
-        legend.selectAll(".legend-background").remove();
+        legend.selectAll(".legend-item").remove()
+        legend.selectAll(".legend-background").remove()
 
         // Bind data and create groups for each author
         const legendItems = legend.selectAll(".legend-item")
@@ -25,20 +25,20 @@ const createLegend = (data) => {
                     .attr("x", 15) // Space from circle
                     .attr("y", 5)
                     .text(d)
-                    .style("fill", colorScale);
+                    .style("fill", colorScale)
 
-                const textWidth = textElem.node().getComputedTextLength(); 
+                const textWidth = textElem.node().getComputedTextLength()
 
                 // Check if the next item would exceed max width
                 if (xPosition + textWidth + 20 > width) {
-                    xPosition = margin.left; 
-                    yPosition += rowHeight;
-                    usedRows++;
+                    xPosition = margin.left * 1.25 
+                    yPosition += rowHeight
+                    usedRows++
                 }
 
-                d3.select(this).attr("transform", `translate(${xPosition}, ${yPosition})`);
-                xPosition += textWidth + 35; 
-            });
+                d3.select(this).attr("transform", `translate(${xPosition}, ${yPosition})`)
+                xPosition += textWidth + 55 
+            })
 
         const background = legend.append("rect")
             .attr("class", "legend-background")
@@ -56,18 +56,18 @@ const createLegend = (data) => {
             
 
         // Update legend background height
-        const legendHeight = usedRows * rowHeight;
+        const legendHeight = usedRows * rowHeight
         background
             .attr("height", legendHeight + 5)
             .attr("width", width)
 
         //Sets the width of the legend to be as wide as the container(from chat)
-        const containerWidth = div.node().getBoundingClientRect().width;
+        const containerWidth = div.node().getBoundingClientRect().width
 
-        legend.attr("viewBox", `0 0 ${containerWidth} ${legendHeight + legendPadding}`);
+        legend.attr("viewBox", `0 0 ${containerWidth} ${legendHeight + legendPadding}`)
 
     }
 
-    drawLegend();
+    drawLegend()
 
 }
