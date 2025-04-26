11449bd2d8770371f38958fc7dcc9681c88c738c
Clara Sønderborg <auso@itu.dk>
2025-04-26 14:24:53 +0200

    repoGitLog to fetch and parse git log data, does not return metadata but only csv at the moment

--- repoGitLog/main.go
+++ repoGitLog/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"net/mail"
@@ -9,23 +10,78 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
 
+// default version. Overridden at build time
+var version = "dev"
+
+func main(){
+	var repoPath = flag.String("repoPath", "", "Mandatory if dataPath is empty: Path to repository. If more than one then seperate by comma without spacing")
+	var versionFlag = flag.Bool("version", false, "Show version")
+	var excludeFile = flag.String("excludeFile", "", "RegExp on file names to exclude")
+	var excludePath = flag.String("excludePath", "", "RegExp on file paths to exclude")
+	var includeFile = flag.String("includeFile", "", "RegExp on file names to include")
+	var includePath = flag.String("includePath", "", "RegExp on file paths to include")
+
+	var yAxis = flag.String("yAxis", "", "Mandatory: Metric for y-axis either churn, commit or growth. churn = linesAdded + linesDeleted, growth = linesAdded - linesDeleted")
+	var nodeSize = flag.String("nodeSize", "", "Mandatory: Metric for node size either churn, commit or growth")
+
+
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println("Version:", version)
+		os.Exit(0)
+	}
+
+	var metricOptions = [] string{"churn", "growth", "commit"}
+
+	if(!slices.Contains(metricOptions, *yAxis)){
+		log.Fatal("yAxis argument must be churn, growth or commit")
+	}
+
+	if(!slices.Contains(metricOptions, *nodeSize)){
+		log.Fatal("nodeSize argument must be churn, growth or commit")
+	}
+
+
+	var res [][] string
+	if *repoPath != "" {
+		var allRepos[] string = strings.Split(*repoPath, ",")
+
+
+		for _, item := range allRepos{
+			var rawData = callGitLog(item)
+			parsedData := parseGitLog(rawData, *excludeFile, *excludePath, *includeFile, *includePath, *yAxis, *nodeSize, item)
+
+			res = append(res, parsedData...)
+		}
+
+	}
+
+	var writer = csv.NewWriter(os.Stdout)
+	writer.Write([]string{"repoPath", "date", "author", "fileName", "linesAdded", "linesDeleted", "yAxis", "nodeSize"})
+	writer.WriteAll(res)
+
+	
+
+}
 
 func callGitLog(repositoryPath string) string {
 
-		var script = `git -C %s log --pretty=format:"%%as %%aE %%(trailers:key=Co-authored-by,valueonly,separator=%%x7c)" --numstat --no-merges --no-renames --diff-filter=x`
+	var script = `git -C %s log --pretty=format:"%%as %%aE %%(trailers:key=Co-authored-by,valueonly,separator=%%x7c)" --numstat --no-merges --no-renames --diff-filter=x`
 
-		var cmd = exec.Command("bash", "-c", fmt.Sprintf(script, repositoryPath))
+	var cmd = exec.Command("bash", "-c", fmt.Sprintf(script, repositoryPath))
 
-		var output, err = cmd.CombinedOutput()
-		if err != nil {
-			fmt.Printf("Could not execute command")
-		}
+	var output, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Could not execute command")
+	}
 
-		return string(output)
+	return string(output)
 
 }
 
@@ -82,13 +138,10 @@ func removeDuplicates(list []string) []string {
 }
 
 func parseGitLog(lines string, excludeFile string, excludePath string, includeFile string, includePath string, yAxis string, nodeSize string, repoPath string) [][]string {
-	//fmt.Printf(lines)
-	
 	var timestamp, author, fileName, lineAdd, lineRemove string
 	var blockLineCount int
 	var result = [][]string{}
 	var authors = []string{}
-	//var timeLayout = "2006-01-02"
 
 	for _, l := range strings.Split(lines, "\n") {
 		var lineContent = strings.TrimSpace(l)
@@ -204,17 +257,3 @@ func addFile(filePath string, excludeFile string, excludePath string, includeFil
 }
 
 
-func writeToCSVFile(list [][]string, location string) {
-	var file, err = os.Create(location)
-
-	if err != nil {
-		log.Fatal("Data file could not be created at path: " + location)
-	}
-
-	defer file.Close()
-
-	var writer = csv.NewWriter(file)
-	writer.Write([]string{"repoPath", "date", "author", "fileName", "linesAdded", "linesDeleted", "yAxis", "nodeSize"})
-	writer.WriteAll(list)
-
-}
