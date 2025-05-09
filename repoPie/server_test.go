f58b0728335dffe36554cdba397c34f0bd292070
Clara Sønderborg <auso@itu.dk>
2025-05-09 10:41:51 +0200

    server testing documentation and added a staticFile unit test

--- repoPie/server_test.go
+++ repoPie/server_test.go
@@ -1,13 +1,16 @@
+// server_test.go tests that the two endpoints "/" and "/data.csv" work as expected
+// Source for testing http in Go: https://speedscale.com/blog/testing-golang-with-httptest/
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-// source: https://speedscale.com/blog/testing-golang-with-httptest/
 func TestServeCSVFromMemory(t *testing.T){
 	var data = [][]string{
 		{"repoPath", "date", "author", "fileName", "yAxis", "yAxisMetric", "nodeSize", "nodeSizeMetric"},
@@ -44,6 +47,44 @@ test/repo,2023-10-16,jukl@itu.dk,src/Car.cs,1,commit,9,churn
 	}
 
 	if string(actual) != expected{
+		t.Errorf("Expected \n%s, got \n%s", expected, string(actual))
+	}
+
+
+}
+
+func TestServeStaticFiles(t *testing.T){
+	var data = [][]string{
+		{"repoPath", "date", "author", "fileName", "yAxis", "yAxisMetric", "nodeSize", "nodeSizeMetric"},
+		{"test/repo", "2023-10-16", "auso@itu.dk", "src/Car.cs", "1", "commit", "9", "churn"},
+		{"test/repo", "2023-10-16", "sscv@itu.dk", "src/Car.cs", "1", "commit", "9", "churn"},
+		{"test/repo", "2023-10-16", "astb@itu.dk", "src/Car.cs", "1", "commit", "9", "churn"},
+		{"test/repo", "2023-10-16", "jukl@itu.dk", "src/Car.cs", "1", "commit", "9", "churn"},
+	}
+
+	var expected = `<div id="container">`
+
+	var handler = startServing(data)
+
+	var w = httptest.NewRecorder()
+	var req = httptest.NewRequest("GET","/", nil)
+
+	handler.ServeHTTP(w, req)
+
+	var response = w.Result()
+
+	if response.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code 200, got %d", response.StatusCode)
+	}
+
+	var actual, err = io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(string(actual))
+
+	if strings.Contains(string(actual), expected){
 		t.Errorf("Expected \n%s, got \n%s", expected, string(actual))
 	}
 
