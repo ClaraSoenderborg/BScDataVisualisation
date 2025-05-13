16e0e0b356d809f589a8dd3a6d6de3f766cf8326
Julia <jukl@itu.dk>
2025-05-13 09:22:56 +0200

    comments

--- repoPie/frontend/js/pie.js
+++ repoPie/frontend/js/pie.js
@@ -1,102 +1,130 @@
+/**
+ * Builds a pie chart visualization for a given file node.
+ * Appends a pie chart to the SVG at the specified coordinates using author data to
+ * define pie slices.
+ *
+ */
 const buildPie = (node, svg) => {
+	const {
+		x,
+		y,
+		yearWeek,
+		fileName,
+		authorMap,
+		nodeSize,
+		yAxis,
+		yAxisMetric,
+		nodeSizeMetric,
+	} = node;
 
-    const { x, y, yearWeek, fileName, authorMap, nodeSize, yAxis, yAxisMetric, nodeSizeMetric } = node
+	const leftAxisGroup = svg.select(".leftAxis");
 
-    const leftAxisGroup = svg.select(".leftAxis")
+	// Append a group for the single pie chart and position it
+	const singlePie = leftAxisGroup
+		.append("g")
+		.attr("transform", `translate(${x}, ${y})`)
+		.attr("class", `singlePie ${sanitizeClassName(fileName)}`)
+		// On click show click tooltip, adjust opacity, hide hovertooltip and remove highlights of matching files
+		.on("click", (e, d) => {
+			showTooltipOnClick({
+				e,
+				data: {
+					fileName: fileName,
+					authorMap: authorMap,
+					nodeSize: nodeSize,
+				},
+				svg,
+			});
+			singlePie.style("opacity", 0.5);
+			d3.select(".hoverTooltip").style("visibility", "hidden");
+			d3.selectAll(".highlight-ring").remove();
+		})
+		// On mouse over show hover tooltip and highlight all pies for with matching filename
+		.on("mouseover", (e, d) => {
+			if (d3.select(".clickTooltip").style("visibility") !== "visible") {
+				showTooltipOnHover({
+					e,
+					data: {
+						fileName: fileName,
+						yAxis: yAxis,
+						yAxisMetric: yAxisMetric,
+						authorMap: authorMap,
+						nodeSize: nodeSize,
+						nodeSizeMetric: nodeSizeMetric,
+					},
+					svg,
+				});
+				singlePie.style("opacity", 0.5);
 
-    const singlePie = leftAxisGroup.append("g")
-        .attr("transform", `translate(${x}, ${y})`)
-        .attr("class", `singlePie ${sanitizeClassName(fileName)}`)
-        .on("click", (e, d) => {
+				// Sanitize file name to use as CSS class name
+				const className = sanitizeClassName(fileName);
 
-            showTooltipOnClick(
-                {
-                    e,
-                    data: {
-                        fileName: fileName,
-                        authorMap: authorMap,
-                        nodeSize: nodeSize
-                    },
-                    svg
-                })
-            singlePie.style("opacity", 0.5)
-            d3.select(".hoverTooltip").style("visibility", "hidden")
-            d3.selectAll(".highlight-ring").remove();
+				// Select and highlight all matching pies by adding a circle outline
+				d3.selectAll(`.${className}`)
+					.raise()
+					.each(function () {
+						const pie = d3.select(this);
+						const bbox = this.getBBox();
+						pie
+							.insert("circle", ":first-child")
+							.attr("class", "highlight-ring")
+							.attr("r", bbox.width * 0.75) // slightly bigger than pie
+							.attr("stroke", "magenta")
+							.attr("stroke-width", 2);
+					});
+			}
+		})
+		// On mouse out hide hover tooltip and remove highlights of matching files
+		.on("mouseout", (e, d) => {
+			if (d3.select(".clickTooltip").style("visibility") !== "visible") {
+				d3.select(".hoverTooltip").style("visibility", "hidden");
+				singlePie.style("opacity", 1);
 
-        })
-        .on("mouseover", (e, d) => {
-            if (d3.select(".clickTooltip").style("visibility") !== "visible") {
-                showTooltipOnHover({
-                    e,
-                    data: {
-                        fileName: fileName,
-                        yAxis: yAxis,
-                        yAxisMetric: yAxisMetric,
-                        authorMap: authorMap,
-                        nodeSize: nodeSize,
-                        nodeSizeMetric: nodeSizeMetric,
-                    },
-                    svg
-                })
-                singlePie.style("opacity", 0.5)
+				const className = sanitizeClassName(fileName);
 
-                const className = sanitizeClassName(fileName);
+				d3.selectAll(`.${className}`)
+					.style("opacity", 1)
+					.selectAll(".highlight-ring")
+					.remove();
+			}
+		});
 
-                // Select all matching single donuts
-                d3.selectAll(`.${className}`).raise()
-                    .each(function () {
-                        const pie = d3.select(this);
-                        const bbox = this.getBBox();
-                        pie.insert("circle", ":first-child")
-                            .attr("class", "highlight-ring")
-                            .attr("r", bbox.width * 0.75) // slightly bigger than donut
-                            .attr("stroke", "magenta")
-                            .attr("stroke-width", 2)
-                    });
-            }
+	// Generate pie slice angles based on node size and apply to the authorMap data
+	var pie = d3
+		.pie()
+		.sort(null)
+		.value(([key, value]) => value.get("nodeSize"));
+	const preparedPie = pie(authorMap);
 
-        })
-        .on("mouseout", (e, d) => {
-            if (d3.select(".clickTooltip").style("visibility") !== "visible") {
-                d3.select(".hoverTooltip").style("visibility", "hidden")
-                singlePie.style("opacity", 1)
+	// Draws the full pie chart with slices for author contribution
+	const drawPie = () => {
+		var arcGen = d3.arc().innerRadius(0).outerRadius(rScale(nodeSize));
 
-                const className = sanitizeClassName(fileName);
+		// Binding the data for pie arc groups
+		var arcs = singlePie
+			.selectAll()
+			.data(preparedPie)
+			.join("g")
+			.attr("class", "pie-arc");
 
-                d3.selectAll(`.${className}`)
-                    .style("opacity", 1)
-                    .selectAll(".highlight-ring").remove();
-            }
+		// Remove previous paths
+		arcs.selectAll("path").remove();
 
-        })
+		// Draw pie slices and color them by author
+		arcs
+			.append("path")
+			.attr("d", arcGen)
+			.attr("fill", (d) => colorScale(d.data[0]));
+	};
 
-    var pie = d3.pie().sort(null).value(([key, value]) => value.get("nodeSize"))
+	drawPie();
+};
 
-    const preparedPie = pie(authorMap)
-
-    const drawPie = () => {
-        var arcGen = d3.arc()
-            .innerRadius(0)
-            .outerRadius(rScale(nodeSize))
-
-        var arcs = singlePie.selectAll()
-            .data(preparedPie)
-            .join("g")
-            .attr("class", "pie-arc")
-
-        arcs.selectAll("path").remove()
-
-        arcs.append("path")
-            .attr("d", arcGen)
-            .attr("fill", d => colorScale(d.data[0]))
-    }
-
-    drawPie()
-
-}
-
+/**
+ * Function to sanitizes a string to use as CSS classname by replacing invalid characters
+ * with underscores.
+ *
+ */
 function sanitizeClassName(str) {
-    return str.replace(/[^a-zA-Z0-9-_]/g, "_")
+	return str.replace(/[^a-zA-Z0-9-_]/g, "_");
 }
-
-
--- repoPie/frontend/js/scales.js
+++ repoPie/frontend/js/scales.js
@@ -1,81 +1,75 @@
-// Chapter 3, Helge book.
-const yScale = d3.scaleLog()
-const xScale = d3.scaleBand()
-const rScale = d3.scaleSqrt() // Helge bog chapter 7
+/**
+ * Defines and sets the scales for the x-axis, y-axis, pie radius, and color scale based on the provided data.
+ *
+ */
 
+// Source: E. Meeks, and A. Dafour, ``D3.js in action'', Third edition, pp 98-101, 2024.
+const xScale = d3.scaleBand();
+//Source:  E. Meeks, and A. Dafour, ``D3.js in action'', Third edition, pp 259-262, 2024.
+const yScale = d3.scaleLog();
+const rScale = d3.scaleSqrt(); // Pie chart radius scale
+
+// Defines the range of colors used
 const colorScale = d3.scaleOrdinal([
+	"#006d77", // Current
+	"#ba274a", // Rose red
+	"#AEBF89", // Yellow green
+	"#8da0cb", // Blue
+	"#e89ff0", // Light purple
+	"#d98b19", // Mustard
+	"#ffa69e", // Melon
+	"#473c85", // Dark blue
+	"#fde12d", // School bus yellow
+	"#628b35", // Avocado
+	"#e96a38", // Coral
+]);
 
-    "#006d77", // Current
-    "#ba274a", // Rose red
-    "#AEBF89", // Yellow green
-    "#8da0cb", // Blue
-    "#e89ff0", // Light purple
-    "#d98b19", // Mustard
-    "#ffa69e", // Melon
-    "#473c85", // Dark blue
-    "#fde12d", // School bus yellow
-    "#628b35", // Avocado
-    "#e96a38", // Coral
-    
+// Sets background colours for graph
+const backgroundColor1 = "#FFFFFF"; // White
+const backgroundColor2 = "#D3D3D3"; // Gray
 
-    /*"#006D77", // Current 
-    "#BA274A", // Rose Red 
-    "#628B35", // Avocado 
-    "#D98B19", // Mustard 
-    "#FFA69E", // Melon 
-    "#8DA0CB", // Blue 
-    "#FDE12D", // School bus yellow 
-    "#853570", // Violet Dark 
-    "#7EBDC3", // Blue 
-    "#46351D", // Shit brown 
-    "#3DA5D9", // Blue 
-    "#E96A38", // Coral*/
-])
+// Configures the domain and range for the scales
+const defineScales = ({
+	data,
+	globalyMax,
+	globalyMin,
+	globalNodeMax,
+	authors,
+	maxNumberOfFiles,
+}) => {
+	// Find weeks for x-axis considering the year for correct order of weeks
+	// Source: ChatGPT (OpenAI), April 2025
+	const minDate = d3.min(data, (d) => d.date);
+	const maxDate = d3.max(data, (d) => d.date);
 
-const backgroundColor1 = "#FFFFFF" // White
-const backgroundColor2 = "#D3D3D3" // Gray
+	const yearWeeks = [];
+	var current = d3.utcMonday(minDate);
+	const end = d3.utcMonday(maxDate);
 
+	while (current <= end) {
+		yearWeeks.push(formatISOWeek(current));
+		current = d3.utcMonday.offset(current, 1);
+	}
 
+	// Sets the xScale domain to the weeks and range to the width of the graph
+	xScale.domain(yearWeeks).range([0, width]);
 
-const defineScales = ({data, globalyMax, globalyMin, globalNodeMax, authors, maxNumberOfFiles}) => {
+	// Sets the yScale domain based on the user chosen metrics minimum and maximum value,
+	// range as the graph height and with logarithmic scale with base of 10 or 2 based on the size of max value
+	yScale
+		.domain([Math.max(1, globalyMin) / 1.5, globalyMax * 1.5])
+		.range([graph_height, margin.top])
+		.base(globalyMax > 100 ? 10 : 2);
 
-    // Find weeks for xScale
-    // source: ChatGPT 
-    const minDate = d3.min(data, d => d.date)
-    const maxDate = d3.max(data, d => d.date)
+	// Calculates the maximum radius for the pie chart based on the number of files shown
+	const radiusMax =
+		maxNumberOfFiles > 10
+			? d3.min([xScale.bandwidth() / 4, graph_radius])
+			: graph_radius;
 
-    
-    const yearWeeks = []
-    var current = d3.utcMonday(minDate) 
-    const end = d3.utcMonday(maxDate)
+	// Sets the rScale domain based on the user chosen metric max value and range based on radiusMax
+	rScale.domain([0, globalNodeMax]).range([radiusMax * 0.5, radiusMax]);
 
-
-    while (current <= end) {
-        yearWeeks.push(formatISOWeek(current))
-        current = d3.utcMonday.offset(current, 1) 
-    }
-
-
-    xScale
-        .domain(yearWeeks) 
-        .range([0, width]) 
-
-    yScale
-        .domain([Math.max(1, globalyMin) / 1.5, globalyMax * 1.5]) 
-        .range([graph_height, margin.top])
-        .base(globalyMax > 100 ? 10 : 2)
-
-
-    const radiusMax = maxNumberOfFiles > 10 ? d3.min([xScale.bandwidth() / 4, graph_radius]) : graph_radius
-
-    rScale
-        .domain([0, globalNodeMax])
-        .range([radiusMax * 0.50, radiusMax])//.base(10) 
-    
-    colorScale
-        .domain(authors)
-
-}
-
-
-
+	// Sets the color scale domain based the authors
+	colorScale.domain(authors);
+};
